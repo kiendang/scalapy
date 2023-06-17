65ec6ef33d6a8784e456da3d7ade972d73bad9ba
Kien Dang <mail@kien.ai>
2023-06-18 02:52:09 +0800

    Tweaks

--- python-native-libs-test/native/src/test/scala/ai/kien/python/PythonSuite.scala
+++ python-native-libs-test/native/src/test/scala/ai/kien/python/PythonSuite.scala
@@ -1,9 +1,9 @@
 package ai.kien.python
 
-import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap, Suite}
+import org.scalatest.{BeforeAndAfterAll, Suite}
 
-trait PythonSuite extends Suite with BeforeAndAfterAllConfigMap {
-  override def beforeAll(configMap: ConfigMap) = {
+trait PythonSuite extends Suite with BeforeAndAfterAll {
+  override def beforeAll() = {
     Config.pythonExecutable.foreach(System.setProperty("scalapy.python.programname", _))
   }
 }
