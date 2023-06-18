6874bb49eb724ffb690c04189fb9559bcb535fad
Kien Dang <mail@kien.ai>
2023-06-18 13:50:19 +0800

    Fix venv activate script location for windows
    https://docs.python.org/3/library/venv.html#how-venvs-work

--- scripts/with_virtualenv.sh
+++ scripts/with_virtualenv.sh
@@ -5,12 +5,24 @@ set -e
 VENV_DIR=$(mktemp -d)
 PYTHON_TEST_PACKAGE="typing-extensions"
 
-echo "Virtualenv created at ${VENV_DIR}"
+echo "CREATING VIRTUALENV AT ${VENV_DIR} ..."
 
 python3 -m venv "${VENV_DIR}"
 "${VENV_DIR}"/bin/python -m pip install "$PYTHON_TEST_PACKAGE"
 
-source "${VENV_DIR}/bin/activate"
+VENV_ACTIVATE_NIX="${VENV_DIR}/bin/activate"
+VENV_ACTIVATE_WIN="${VENV_DIR}/Scripts/activate.bat"
+
+if [ -f "$VENV_ACTIVATE_NIX" ]; then
+  source "$VENV_ACTIVATE_NIX"
+elif [ -f "$VENV_ACTIVATE_WIN" ]; then
+  cmd "$VENV_ACTIVATE_WIN"
+else
+  echo "COULD NOT FIND VIRTUALENV ACTIVATE SCRIPT AT EITHER ${VENV_ACTIVATE_NIX} OR ${VENV_ACTIVATE_WIN}."
+  exit 1
+fi
+
+echo "RUNNING TESTS ..."
 
 sbt \
   -Dscalapy.ci.executable="${VENV_DIR}/bin/python" \
@@ -18,5 +30,5 @@ sbt \
   "$@"
 
 deactivate
-echo "Delete virtualenv ${VENV_DIR}"
+echo "DELETING VIRTUALENV ${VENV_DIR} ..."
 rm -rf "${VENV_DIR}"
