41e1c9aac8a4924833ce69dbb861ccf306e55d08
Kien Dang <mail@kien.ai>
2023-06-18 14:12:56 +0800

    Fix virtualenv python executable for windows

--- scripts/with_virtualenv.sh
+++ scripts/with_virtualenv.sh
@@ -8,7 +8,20 @@ PYTHON_TEST_PACKAGE="typing-extensions"
 echo "CREATING VIRTUALENV AT ${VENV_DIR} ..."
 
 python3 -m venv "${VENV_DIR}"
-"${VENV_DIR}"/bin/python -m pip install "$PYTHON_TEST_PACKAGE"
+
+PYTHON_NIX="${VENV_DIR}/bin/python"
+PYTHON_WIN="${VENV_DIR}/Scripts/python.exe"
+
+if [ -f "$PYTHON_NIX" ]; then
+  PYTHON="$PYTHON_NIX"
+elif [ -f "$PYTHON_WIN" ]; then
+  PYTHON="$PYTHON_WIN"
+else
+  echo "COULD NOT FIND VIRTUALENV PYTHON EXECUTABLE AT EITHER ${PYTHON_NIX} OR ${PYTHON_WIN}."
+  exit 1
+fi
+
+"$PYTHON" -m pip install "$PYTHON_TEST_PACKAGE"
 
 VENV_ACTIVATE_NIX="${VENV_DIR}/bin/activate"
 VENV_ACTIVATE_WIN="${VENV_DIR}/Scripts/activate.bat"
